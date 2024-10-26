685af13bb65c388972ea8cb6d70e6498e690b2d6
nikkigalforte <nikimnsn77@gmail.com>
2024-10-26 10:44:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿function countWords() {
+    const sentence = document.getElementById("sentence").value;
+    let wordCount = 0;
+    const space = ' ';
+
+    for (let i = 0; i < sentence.length; i++) {
+        const char = sentence[i];
+        if (char !== space) {
+            if (i === 0 || sentence[i - 1] === space) {
+                wordCount++;
+            }
+        }
+    }
+
+    document.getElementById("wordCount").innerText = `Количество слов: ${wordCount}`;
+}
